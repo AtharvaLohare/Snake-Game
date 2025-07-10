3252e82c95e6b6e08f032512cf82ed64834fa234
Atharva-Gajendra-Lohare <atharvlohare@gmail.com>
2025-07-10 23:33:39 +0530

    Added the food class along with graphics and Sound files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,87 @@
 #include <raylib.h>
+#include <iostream>
+#include <deque>
+using namespace std;
+
+Color green = {173, 204, 96, 255};
+Color darkgreen = {43, 51, 24, 255};
+
+int cellSize = 30;
+int cellCount = 25;
 
 
-int main() 
-{
+
+
+
+
+
+
+
+
+class Food{
+
+public:
+    /* Members required to describe the food bite.*/
+    Vector2 position = GeneratRandomPosition();
+    Texture2D texture;
+
+    /* Rendering the texture to be used as food, 
+       Custom Constructor this is. */
+    Food(){
+        Image image = LoadImage("Graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+    }
+
+    /* The Custom destructir used to unload the texture 
+       as it is not required with the end of program execution.*/
+    ~Food(){
+        UnloadTexture(texture);
+    }
+
+    /* To draw the food in each iteration at a random position.*/
+    void Draw(){
+        DrawTexture(texture, position.x*cellSize, position.y*cellSize, WHITE);
+
+    }
+
+    /* Generates the randome position for the food in each iteration.*/
+    Vector2 GeneratRandomPosition(){
+
+        float x = GetRandomValue(0, cellCount-1);
+        float y = GetRandomValue(0, cellCount-1);
+
+        Vector2 newPosition = {x, y};
+        return newPosition;
+    }
+};
+
+
+
+
+
+
+
+
+int main() {   
     
+    InitWindow(cellSize*cellCount, cellSize*cellCount, "Retro Snake");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false){
+
+
+
+        /* begin drawing for the new iteration of the gameloop.*/
+        BeginDrawing();
+
+        /* clear background */
+        ClearBackground(green);
+
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+    return 0;
 }
